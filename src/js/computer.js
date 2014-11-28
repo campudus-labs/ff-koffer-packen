f3087627b86d36ac495abdc6336aaabc83eed243
Joern Bernhardt <jb@campudus.com>
2014-11-28 11:26:10 +0100

    basic layout
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

---
+++ src/js/computer.js
@@ -0,0 +1,4 @@
+
+function shuffleInputElements(elementCount) {
+  return [];
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 
+var elemente = require('./elemente.json');
 var calc = require('./calculator');
 
 var summe = calc.mult(3, calc.sum(1, 5));
 
 console.log(summe);
+console.log('elemente:');
+console.log(elemente);
+
