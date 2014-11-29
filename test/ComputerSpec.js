d38af30d2fdc94bd3ab7d0387e6816a7f9a215d2
Joern Bernhardt <jb@campudus.com>
2014-11-29 23:23:51 +0100

    initial tests for the suitcase
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- test/ComputerSpec.js
+++ test/ComputerSpec.js
@@ -3,7 +3,7 @@ describe('The computer-shuffle of elements', function () {
   var computer = require('../src/js/computer');
 
   it('should take as much elements as expected', function () {
-    expect(computer.shuffle(5).length).toBe(5);
+    expect(computer.shuffle(3).length).toBe(3);
   });
 
   it('should never take more elements than we have in elements', function () {
@@ -14,7 +14,9 @@ describe('The computer-shuffle of elements', function () {
 
   it('should not have duplicates', function () {
     var elements = computer.shuffle(100);
-    elements.forEach(function (idx, elem) {
+    elements.forEach(function (elem, idx) {
+      console.log(idx);
+      console.log(elem);
       expect(idx).toBe(elements.lastIndexOf(elem));
     })
   });
