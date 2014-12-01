5badf885639b21ed0f75da4572373cf4836e4d8d
Konstantin Axt <axtkonstantin@hotmail.de>
2014-12-01 09:33:57 +0100

    shuffling the elements

--- src/js/computer.js
+++ src/js/computer.js
@@ -1,6 +1,20 @@
 
+var elementCount = require('./elemente.json');
+
 function shuffleInputElements(elementCount) {
-  return [];
+
+  var current, random;
+
+  for( var i = 0; i <= elementCount.length; i++  ){
+
+      random = Math.floor(Math.random() * elementCount.length);
+      current = elementCount[i];
+      elementCount[i] = elementCount[random];
+      elementCount[random] = current;
+
+  }
+
+  return current; // array
 }
 
 module.exports = {
